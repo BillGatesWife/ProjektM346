6869650afa4747abc9726d4751ab83d7767d30ab
Lukas Gusset <blabla@gmail.com>
2023-12-13 15:16:37 +0100

    script test

--- Website/script.js
+++ Website/script.js
@@ -5,3 +5,35 @@ document.querySelector('.upload-btn-wrapper input[type=file]').addEventListener(
     }
   });
   
+// Load the AWS SDK for JavaScript
+var AWS = require('aws-sdk');
+
+// Set the region
+AWS.config.update({region: 'us-east-1'});
+
+// Create an S3 service object
+var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Set the bucket name and key
+var bucketName = 'ImagesToResize';
+var keyName = 'KEY';
+
+// Create a new FormData object
+var formData = new FormData();
+
+// Add the file to the FormData object
+formData.append('file', file);
+
+// Upload the file to the S3 bucket
+s3.upload({
+    Bucket: bucketName,
+    Key: keyName,
+    Body: file,
+    ACL: 'public-read'
+}, function(err, data) {
+    if (err) {
+        console.log('Error uploading file:', err);
+    } else {
+        console.log('File uploaded successfully:', data.Location);
+    }
+});
