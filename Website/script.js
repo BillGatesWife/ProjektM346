dacdffd31fb3ec2c51e67a00665f9a7bc676f7f8
Lukas Gusset <blabla@gmail.com>
2023-12-13 14:52:11 +0100

    moved script

---
+++ Website/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.upload-btn-wrapper input[type=file]').addEventListener('change', function() {
+    if (this.files && this.files.length > 0) {
+      // Update button text or perform other actions
+      document.querySelector('.btn').textContent = this.files[0].name;
+    }
+  });
+  
