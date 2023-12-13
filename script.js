07f7c8cd767d2f484424e3244480c7b953753d8b
Mihajlo Marinkovic <mihajlo.marinkovic@bbcag.ch>
2023-12-13 15:30:49 +0100

    hallo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector('.upload-btn-wrapper input[type=file]').addEventListener('change', function() {
     if (this.files && this.files.length > 0) {
       // Update button text or perform other actions
-      document.querySelector('.btn').textContent = this.files[0].name;
+      document.querySelector('.file-info').textContent = this.files[0].name;
     }
   });
   
